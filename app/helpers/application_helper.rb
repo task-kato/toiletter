701df9d627c266821f1163cad0183e63b6241dd8
tasuku <dptk.099@gmail.com>
2021-06-08 17:43:42 +0900

    feat(*): add user_top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def document_title 
+        if @title.present? 
+            "#{@title}"
+        else
+            "Toiletter"
+        end
+    end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@fortawesome/fontawesome-free/js/all'
+import '../stylesheets/staff.css'
+import '../stylesheets/user.css'
+
 
 Rails.start()
 Turbolinks.start()
