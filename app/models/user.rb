b7e301d69cb966c63bfe1887aba65906b4a145f4
tasuku <dptk.099@gmail.com>
2021-06-14 14:21:08 +0900

    feat(*): modify user top_page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+    validates :email, uniqueness: true, presence: true
 end
--- db/migrate/20210607050256_create_users.rb
+++ db/migrate/20210607050256_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :name, null: false # 名前
+      t.string :name_kana, null: false # 名前（カナ）
+      t.string :nick_name, null: false # ニックネーム
+      t.integer :gender, null: false, default: 1 # 1: 男性, 2: 女性
+      t.string :email, null: false, unique: true # メールアドレス
+      t.string :hashed_password # パスワード
+      t.boolean :suspended, null: false, default: false # 停止フラグ
 
       t.timestamps
     end
