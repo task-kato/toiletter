1f7eb76cba7ad3c64cdd4dc1ad1fba318aa9500a
tasuku <dptk.099@gmail.com>
2021-07-11 18:40:10 +0900

    feat(*): add error_handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  include ErrorHandlers if Rails.env.production?
+
   private def set_layout
     if params[:controller] =~ %r{\A(staff|user)/}
       Regexp.last_match[1]
