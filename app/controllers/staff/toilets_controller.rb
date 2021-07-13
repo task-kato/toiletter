0835b4a0faee3892f9ebe0a216c5139107a99bb4
tasuku <dptk.099@gmail.com>
2021-07-13 15:35:20 +0900

    chore(*): modify google map api

--- app/controllers/staff/toilets_controller.rb
+++ app/controllers/staff/toilets_controller.rb
@@ -1,6 +1,7 @@
 class Staff::ToiletsController < Staff::Base
 
     def index
-        
+        @toilet = Toilet.first
+        gon.toilet = @toilet
     end
 end
---
+++ db/seeds/development/toilets.rb
@@ -0,0 +1,4 @@
+Toilet.create!(
+    name: '自宅',
+    address: '千葉県大網白里市季美の森2-47-2'
+)
